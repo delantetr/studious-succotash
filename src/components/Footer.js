444805261eb7fbe7e762249e95754ad90827482f
Delante Randolph <delantetr@MacBook-Pro.local>
2023-08-04 05:48:37 -0400

    added contact form and resume code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faStackOverflow, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <footer>
-      <ul>
-      <li><a href="https://github.com/delantetr">GitHub</a></li>
-      <li><a href="link to LinkedIn profile">LinkedIn</a></li>
-      <li><a href="link to third platform profile">Third Platform</a></li>
-      </ul>
+      <div className="container text-center">
+        <p>
+          Connect with me:
+          <br />
+          <a href="https://github.com/your-github-profile" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+          <a href="https://linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
+        </p>
+      </div>
     </footer>
   );
 };
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,24 +1,132 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Contact() {
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const [errors, setErrors] = useState({
+    name: '',
+    email: ''
+  });
+
+  const [submitted, setSubmitted] = useState(false)
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleBlur = (event) => {
+    const { name, value } = event.target;
+    if (value.trim() === '') {
+      setErrors({
+        ...errors,
+        [name]: 'This field is required'
+      });
+    } else {
+      setErrors({
+        ...errors,
+        [name]: ''
+      });
+    }
+  };
+
+  const validateEmail = (email) => {
+    const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return pattern.test(email);
+  };
+
+  const handleEmailBlur = () => {
+    const { email } = formData;
+    if (email.trim() !== '' && !validateEmail(email)) {
+      setErrors({
+        ...errors,
+        email: 'Invalid email address'
+      });
+    } else {
+      setErrors({
+        ...errors,
+        email: ''
+      });
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Form submitted:', formData);
+    setSubmitted(true);
+  };
+
   return (
     <div>
       <h1>Contact</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+      {submitted ? (
+        <div className="alert alert-success" role="alert">
+          Thank you! Your form has been submitted.
+        </div>
+      ) : (
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            onBlur={handleBlur}
+            required
+          />
+          {errors.name && <p className="text-danger">{errors.name}</p>}
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            onBlur={handleEmailBlur}
+            required
+          />
+          {errors.email && <p className="text-danger">{errors.email}</p>}
+        </div>
+        <div className="mb-3">
+          <label htmlFor="message" className="form-label">
+            Message
+          </label>
+          <textarea
+            className="form-control"
+            id="message"
+            name="message"
+            rows="4"
+            value={formData.message}
+            onChange={handleInputChange}
+            onBlur={handleBlur}
+            required
+          />
+          {errors.message && <p className="text-danger">{errors.message}</p>}
+        </div>
+        <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      )}
     </div>
   );
-}
+};
+
+export default Contact;
